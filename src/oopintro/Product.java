249e29bc98e18066af618941e2b7112a533efb2a
Erdogan PACACI <pacacierdogan@gmail.com>
2021-06-03 09:47:06 +0300

    Product nesnesine getter ve Setter lar eklendi

---
+++ src/oopintro/Product.java
@@ -0,0 +1,12 @@
+package oopintro;
+
+public class Product {
+
+	private int id;
+	private String name;
+	private double unitPrice;
+	private int discount;
+	private double unitPriceAfterDiscount;
+
+	
+	
