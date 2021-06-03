72dd6a4d4306a271c7c4f9c6a0a5ea512ef5eef0
Erdogan PACACI <pacacierdogan@gmail.com>
2021-06-03 10:05:41 +0300

    readme güncellendi

--- src/oopintro/Product.java
+++ src/oopintro/Product.java
@@ -7,6 +7,45 @@ public class Product {
 	private double unitPrice;
 	private int discount;
 	private double unitPriceAfterDiscount;
-
 	
+
+	public void setId(int id) {
+		this.id=id;
+	}
+	
+	public int getId() {
+		return this.id;
+	}
+	
+	
+	public void setName(String name) {
+		this.name=name;
+	}
+	public String getName() {
+		return this.name;
+	}
+	
+	
+	public void setUnitPrice(double unitPrice) {
+		this.unitPrice=unitPrice;
+	}
+	public double getUnitPrice() {
+		return this.unitPrice;
+	}
+	
+	public void setDiscount(int discount) {
+		this.discount=discount;
+	}
+	public int  getDiscount() {
+	  return this.discount;
+	}
+	
+	
+	public double getUnitPriceAfterDiscount() {
+		
+		return this.unitPrice - (this.unitPrice * this.discount / 100);
+	}
+	
+
+}
 	
